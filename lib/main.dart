6b69298f5c5d4aaff1266d5f5454d891db3fb68e
Luqman Abdurrohman <luqman.abdurrohman@freshconsulting.com>
2024-02-07 20:14:41 -0800

    feature progress: gamepad UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 
 void main() {
   runApp(const MyApp());
@@ -30,12 +33,24 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  BluetoothAdapterState _adapterState = BluetoothAdapterState.unknown;
+  late StreamSubscription<BluetoothAdapterState> _adapterStateStateSubscription;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
+  @override
+  void initState() {
+    super.initState();
+    _adapterStateStateSubscription = FlutterBluePlus.adapterState.listen((state) {
+      _adapterState = state;
+      if (mounted) {
+        setState(() {});
+      }
     });
+  }
+
+  @override
+  void dispose() {
+    _adapterStateStateSubscription.cancel();
+    super.dispose();
   }
 
   @override
@@ -56,17 +71,11 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         child: Row(
           children: [
-            /// TODO: implement the manuever controls UI
-            Flexible(
+            const Flexible(
               flex: 4,
-              child: Container(
-                color: Colors.blue,
-              ),
+              child: ControlPad(),
             ),
-
             const Spacer(),
-
-            /// TODO: implement the speed control UI
             Flexible(
               flex: 4,
               child: Container(
@@ -79,3 +88,86 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+
+class ControlPad extends StatelessWidget {
+  const ControlPad({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        ControlPadBtn(
+          icon: Icons.arrow_upward_outlined,
+          onTap: () {
+            print("FORWARD");
+          },
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ControlPadBtn(
+              icon: Icons.arrow_back_outlined,
+              onTap: () {
+                print("LEFT");
+              },
+            ),
+            Container(
+              height: 60,
+              width: 60,
+              color: Colors.black,
+            ),
+            ControlPadBtn(
+              icon: Icons.arrow_forward_ios_outlined,
+              onTap: () {
+                print("RIGHT");
+              },
+            ),
+          ],
+        ),
+        ControlPadBtn(
+          icon: Icons.arrow_downward_outlined,
+          onTap: () {
+            print("REVERSE");
+          },
+        ),
+      ],
+    );
+  }
+}
+
+class ControlPadBtn extends StatelessWidget {
+  const ControlPadBtn({super.key, this.icon, this.onTap});
+
+  final IconData? icon;
+  final Function()? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        height: 60,
+        width: 60,
+        decoration: const BoxDecoration(color: Colors.black),
+        child: Icon(
+          icon,
+          size: 24,
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+}
+
+
+/*
+Class called RemoteControllerService
+ - functions such as forward, backward, left, right (send data to arduino via bluetooth) 
+ - should not know about the bluetooth implementation
+ - we should inject an abstracted class that is a bluetooth implementation
+
+Abstracted class called BluetoothService
+ - 
+
+*/
